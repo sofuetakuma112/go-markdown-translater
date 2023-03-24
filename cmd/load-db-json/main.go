241f74383dcd3973deb6fc8ebdcc8eaf2a8c1483
sofuetakuma <kawahagi0620@gmail.com>
2023-03-24 20:58:29 +0900

    プログレスバーを表示するよう修正

--- cmd/load-db-json/main.go
+++ cmd/load-db-json/main.go
@@ -6,7 +6,9 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"os"
 
+	"github.com/cheggaaa/pb/v3"
 	_ "github.com/mattn/go-sqlite3"
 )
 
@@ -17,6 +19,14 @@ type Item struct {
 }
 
 func main() {
+	// ファイルが存在するかどうかチェック
+	if _, err := os.Stat("translations.db"); err == nil {
+		// ファイルを削除
+		if err := os.Remove("translations.db"); err != nil {
+			panic(err)
+		}
+	}
+
 	// SQLiteデータベースに接続する
 	db, err := sql.Open("sqlite3", "translations.db")
 	if err != nil {
@@ -47,13 +57,23 @@ func main() {
 		log.Fatal(err)
 	}
 
+	// ループの回数を指定する
+	count := len(items)
+	// pbの設定
+	bar := pb.StartNew(count)
+	bar.SetMaxWidth(80)
+	bar.Set(pb.Bytes, true)
 	// データベースにItemを挿入する
 	for _, item := range items {
 		_, err = db.Exec("INSERT INTO translations (source_text, translated_text, formatted_text) VALUES (?, ?, ?)", item.SourceText, item.TranslatedText, item.FormattedText)
 		if err != nil {
 			log.Fatal(err)
 		}
+
+		bar.Increment()
 	}
+
+	bar.Finish()
 
 	fmt.Println("Inserted", len(items), "items into the database")
 }
